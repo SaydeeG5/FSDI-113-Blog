54ba239e011bbc2ecc4fdac8081440fd1fe60875
Maria Saydee Guevarra <saydeeguevarra@gmail.com>
2023-02-24 21:21:23 -0800

    Added buttons

--- posts/urls.py
+++ posts/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path("", PostListView.as_view(), name="list"),
     path("<int:pk>/edit/", PostUpdateView.as_view(), name="edit"),
     path("<int:pk>/delete/", PostDeleteView.as_view(), name="delete"),
-    path("detail/", PostDetailView.as_view(), name="detail"),
+    path("<int:pk>/", PostDetailView.as_view(), name="detail"),
     path("new/", PostCreateView.as_view(), name="new"),
 ]
